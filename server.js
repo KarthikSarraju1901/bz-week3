5e6037036d1f58bbe0b3872771f4251570a7f7b2
KarthikSarraju1901 <srinath.sarraju@gmail.com>
2020-01-08 12:22:24 +0530

    serverupdated

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path=  require('path')
+const port = process.env.PORT|| 3000
+const app =express()
+app.use(express.static(__dirname + '/dist/ngproject'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/dist/ngproject/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
